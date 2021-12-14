7f7c1fa5d6d41c72035c758556ceffc83dc3f878
KimrAndrew <andrewrk1997@gmail.com>
2021-12-13 19:55:17 -0800

    added test_enqueue_one

--- python/tests/test_stack_queue_pseudo.py
+++ python/tests/test_stack_queue_pseudo.py
@@ -2,3 +2,13 @@ from stack_queue_pseudo.stack_queue_pseudo import Queue
 
 def test_import():
     assert Queue()
+
+def test_enqueue_one():
+    pq = Queue()
+    pq.enqueue(1)
+
+    expected = 1
+    actual = pq._stack_one.peek()
+    assert expected == actual
+
+
