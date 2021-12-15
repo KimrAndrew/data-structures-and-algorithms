7f3a2f21351c6ba037b4dab58914d5c7286b07cc
KimrAndrew <andrewrk1997@gmail.com>
2021-12-14 22:52:54 -0800

    adds class Animal and subclasses Dog and Cat

---
+++ python/stack_queue_animal_shelter/stack_queue_animal_shelter.py
@@ -0,0 +1,12 @@
+from stack_and_queue.stack_and_queue import Node, Stack, Queue
+
+class Animal():
+    pass
+
+class Dog(Animal):
+    def __str__(self):
+        return 'Dog'
+
+class Cat(Animal):
+    def __str__(self):
+        return 'Cat'
