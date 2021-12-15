28c680c703e355c3548c761126cdf1b8db703a17
KimrAndrew <andrewrk1997@gmail.com>
2021-12-14 22:59:09 -0800

    adds dequeue to AnimalShelter

--- python/stack_queue_animal_shelter/stack_queue_animal_shelter.py
+++ python/stack_queue_animal_shelter/stack_queue_animal_shelter.py
@@ -22,3 +22,47 @@ class AnimalShelter():
         
     def enqueue(self,value):
         self.line.enqueue(value)
+    
+    # Need to ask what to return if pref not in queue
+    # Need to ask how to handle if called on empty AnimalShelter
+    def dequeue(self,pref=None):
+
+        self.enqueue(PlaceHolder())
+
+        if pref == "cat":
+            if not self.line.is_empty():
+                dq = self.line.dequeue()
+                while not isinstance(dq, Cat) and not isinstance(dq,PlaceHolder):
+                    self.line.enqueue(dq)
+                    dq = self.line.dequeue()
+                if  not isinstance(dq,PlaceHolder):
+                    # get rid of PlaceHolder
+                    current = self.line.dequeue()
+                    # cycle back to and remove PlaceHolder
+                    while not isinstance(current,PlaceHolder):
+                        self.line.enqueue(current)
+                        current = self.line.dequeue()
+                return dq
+            return None
+
+        elif pref == "dog":
+            if not self.line.is_empty():
+                dq = self.line.dequeue()
+                while not isinstance(dq, Dog) and not isinstance(dq,PlaceHolder):
+                    self.line.enqueue(dq)
+                    dq = self.line.dequeue()
+                    self.line.enqueue(dq)
+                if  not isinstance(dq,PlaceHolder):
+                    # get rid of PlaceHolder
+                    current = self.line.dequeue()
+                    # cycle back to and remove PlaceHolder
+                    while not isinstance(current,PlaceHolder):
+                        self.line.enqueue(current)
+                        current = self.line.dequeue()
+                return dq
+            return None
+        else:
+            return None
+            
+
+
