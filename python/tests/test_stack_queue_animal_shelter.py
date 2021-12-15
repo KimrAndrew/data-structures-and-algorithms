f75c751a253c16a3cea238963157c2b2c839c573
KimrAndrew <andrewrk1997@gmail.com>
2021-12-14 22:46:45 -0800

    added tests for imports

--- python/tests/test_stack_queue_animal_shelter.py
+++ python/tests/test_stack_queue_animal_shelter.py
@@ -1,4 +1,13 @@
-from stack_queue_animal_shelter.stack_queue_animal_shelter import AnimalShelter
+from stack_queue_animal_shelter.stack_queue_animal_shelter import AnimalShelter, Animal, Cat, Dog
 
-test_import()
-assert AnimalShelter()
+def test_import():
+    assert AnimalShelter()
+
+def test_dog():
+    assert Dog()
+
+def test_cat():
+    assert Cat()
+
+def test_animal():
+    assert Animal()
