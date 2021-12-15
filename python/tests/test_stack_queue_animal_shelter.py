e0851ae88842db91a9f57a8d7ecb2cea465d39ea
KimrAndrew <andrewrk1997@gmail.com>
2021-12-14 22:49:03 -0800

    adds test_enqueue_to_empty

--- python/tests/test_stack_queue_animal_shelter.py
+++ python/tests/test_stack_queue_animal_shelter.py
@@ -19,3 +19,9 @@ def test_dog_is_animal():
 def test_cat_is_animal():
     cat = Cat()
     assert isinstance(cat,Animal)
+
+def test_enqueue_to_empty():
+    shelter = AnimalShelter()
+    shelter.enqueue(Cat())
+    dq = shelter.line.dequeue()
+    assert isinstance(dq,Cat)
