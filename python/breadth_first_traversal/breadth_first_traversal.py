46ec48e771968d20931852c564b3de9378a382ea
KimrAndrew <andrewrk1997@gmail.com>
2021-12-21 15:23:32 -0800

    breadth_first now passes empty and one high tests

--- python/breadth_first_traversal/breadth_first_traversal.py
+++ python/breadth_first_traversal/breadth_first_traversal.py
@@ -3,5 +3,17 @@ from trees.trees import BinaryTree
 from stack_and_queue.stack_and_queue import Queue
 
 def breadth_first(tree:BinaryTree) -> list:
+    q = Queue()
+    output = []
     if tree.root is None:
-        return []
+        return output
+    q.enqueue(tree.root)
+    while not q.is_empty():
+        temp = q.dequeue()
+        if temp.left is not None:
+            q.enqueue(temp.left)
+        if temp.right is not None:
+            q.enqueue(temp.right)
+        output.append(temp.value)
+    return output
+    
