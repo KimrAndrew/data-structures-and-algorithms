6fe99c0de6575cc01ad08bd11bcf92ff61518030
KimrAndrew <andrewrk1997@gmail.com>
2021-12-22 17:00:38 -0800

    created function to add Node to KAryTree

---
+++ python/tests/test_tree_fizz_buzz.py
@@ -0,0 +1,52 @@
+from trees.trees import KAryTree, KTreeNode, breadth_first
+
+# def test_tree_add():
+#     tree = KAryTree(3)
+#     tree.add(1)
+#     tree.add(2)
+#     tree.add(3)
+#     tree.add(4)
+#     second_level_left = tree.root.connections[0]
+#     second_level_mid = tree.root.connections[1]
+#     second_level_right = tree.root.connections[2]
+#     tree.add(5)
+#     tree.add(6)
+#     tree.add(7)
+#     tree.add(8)
+#     # tree.add(9)
+#     # tree.add(10)
+#     # tree.add(11)
+#     # tree.add(12)
+#     # tree.add(13)
+#     assert tree.root.value == 1
+#     assert tree.root.connections[0].value == 2
+#     assert tree.root.connections[1].value == 3
+#     assert tree.root.connections[2].value == 4
+#     assert second_level_left.connections[0].value == 5
+#     assert second_level_left.connections[1].value == 6
+#     assert second_level_left.connections[2].value == 7
+#     assert second_level_mid.connections[0].value == 8
+#     # assert second_level_mid.connections[1].value == 9
+#     # assert second_level_mid.connections[2].value == 10
+#     # assert second_level_right.connections[0].value == 11
+#     # assert second_level_right.connections[1].value == 12
+#     # assert second_level_right.connections[2].value == 13
+
+def test_breadth_first():
+    tree = KAryTree(3)
+    tree.root = KTreeNode(3,1)
+    second_level = [KTreeNode(3,2),KTreeNode(3,3),KTreeNode(3,4)]
+    third_level_left = [KTreeNode(3,5),KTreeNode(3,6),KTreeNode(3,7)]
+    third_level_mid = [KTreeNode(3,6),KTreeNode(3,7),KTreeNode(3,8)]
+    third_level_right = [KTreeNode(3,9),KTreeNode(3,10),KTreeNode(3,11)]
+    tree.root.connections = second_level
+    tree.root.connections[0].connections = third_level_left
+    tree.root.connections[1].connections =  third_level_mid
+    tree.root.connections[2].connections = third_level_right
+    breadth_first(tree,12)
+    actual = tree.root.connections[0].connections[0].connections[0].value
+    expected = 12
+    #print([connection.value for connection in tree.root.connections[0].connections])
+    #print(tree.root.connections[0].connections[0].connections[0].value)
+    #print(actual)
+    assert actual == expected
--- python/trees/trees.py
+++ python/trees/trees.py
@@ -1,3 +1,7 @@
+from typing import Collection
+from collections import deque
+
+
 class BTreeNode():
     def __init__(self,value,left=None,right=None):
         self.value = value
@@ -30,6 +34,63 @@ class BinaryTree():
             output.append(root.value)
         walk(self.root)
         return output
-            
 
+class KTreeNode():
 
+    def __init__(self,k:int,value,connections:Collection=[]) -> None:
+            self.connections = list(connections)
+            if len(self.connections) > k:
+                raise IndexError(f'Length of connections: {connections} cannot be greater than k: {k}')
+            self.value = value
+
+class KAryTree():
+    def __init__(self,k:int,root = None) -> None:
+            self.k = k
+            if root is None:
+                self.root = root
+            elif not isinstance(root,KTreeNode):
+                self.root = KTreeNode(root)
+            else:
+                self.root = root
+        
+    # def add(self, value):
+    #     if not isinstance(value,KTreeNode):
+    #         value = KTreeNode(self.k,value)
+    #     if self.root is None:
+    #         self.root = value
+    #         return
+
+    #     def add_to_left(root:KTreeNode,to_add = value):
+    #         if len(root.connections)  < self.k:
+    #             root.connections.append(to_add)
+    #         add_to_left(root.connections[0],to_add)
+
+    #     def find_open_spot(root:KTreeNode,to_add = value):
+    #         if len(root.connections) < self.k:
+    #             print(len(root.connections))
+    #             print('to_add', to_add.value)
+    #             root.connections.append(to_add)
+    #             print([connection.value for connection in root.connections])
+    #             return True
+    #         for node in root.connections:
+    #             find_open_spot(node,to_add)
+
+    #     find_open_spot(self.root)
+    #def add(tree:KAryTree):
+def breadth_first(tree:KAryTree,to_add) -> list:
+    q = deque()
+    if tree.root is None:
+        tree.root = KTreeNode(tree.k, to_add)
+    q.appendleft(tree.root)
+    temp = None
+    while len(q) > 0:
+        temp = q.pop()
+        if len(temp.connections) < tree.k:
+            print(temp.value)
+            print(temp.connections)
+            temp.connections.append(KTreeNode(tree.k,to_add))
+            return
+        for connection in temp.connections:
+            q.appendleft(connection)
+    
+    return
