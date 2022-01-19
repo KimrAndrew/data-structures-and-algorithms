c0a118f5fdcc30bf7da31dbd34cf9402a1622105
KimrAndrew <andrewrk1997@gmail.com>
2022-01-18 21:07:02 -0800

    added test for add_edge with weights

--- python/tests/test_graph.py
+++ python/tests/test_graph.py
@@ -38,4 +38,17 @@ def test_add_edge_no_weight_nosy():
     assert [a,None] in g.vertices[b]
     assert [a,None] in g.vertices[c]
 
-#def test_add_edge():
+def test_add_edge_nosy():
+    g = Graph()
+    a = g.add_node('A')
+    b = g.add_node('B')
+    c = g.add_node('C')
+    g.add_edge(a,b,5)
+    g.add_edge(a,c,2)
+    g.add_edge(b,a,5)
+    g.add_edge(c,a,10)
+
+    assert [b,5] in g.vertices[a]
+    assert [c,2] in g.vertices[a]
+    assert [a,5] in g.vertices[b]
+    assert [a,10] in g.vertices[c]
